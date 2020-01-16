a2243f2148545625ec43ee102fc5a948d4bd5548
Jakub <jakub.oblizajek@gmail.com>
2020-01-16 23:58:31 +0100

    Add resigter account test

--- tests/CartCest.php
+++ tests/CartCest.php
@@ -1,14 +1,36 @@
 <?php
+use Page\Acceptance\Cart;
+
 class CartCest
 {    
-    public function _before(AcceptanceTester $I)
+    public function _before(AcceptanceTester $I, Cart $page)
     {
-        $I->amOnPage('/');
+        $I->maximizeWindow();
+        $I->amOnPage($page::$URL);
         $I->wait(2);
     }
 
-    public function buySuccessfully(AcceptanceTester $I)
+    public function signUpSuccessfully(AcceptanceTester $I, Cart $page)
     {
-        $I->see('Store information');
+        $page->createAccount(
+            'perpetrator@box.pl',
+            'Mr',
+            'John',
+            'Kovalsky',
+            'kowalPass120',
+            '20',
+            '10',
+            '1984',
+            '5 Washington Avenue',
+            'New York',
+            'New York',
+            '00000',
+            'United States',
+            '12345678910',
+            'Basic address'
+        );
+        $I->seeElement('//*[@title="View my customer account"]');
+        $I->see('John Kovalsky');
+
     }
 }
--- tests/_support/Page/Acceptance/Cart.php
+++ tests/_support/Page/Acceptance/Cart.php
@@ -1,10 +1,28 @@
 <?php
 namespace Page\Acceptance;
+use Facebook\WebDriver\WebDriverKeys;
 
 class Cart
 {
     // include url of current page
-    public static $URL = '';
+    public static $URL = '/';
+    public $signInButton = '//*[@class="header_user_info"]/a';
+    public $emailField = '#email_create';
+    public $mrOrMrsField = 'input[name="id_gender"]';
+    public $firstNameField = '#customer_firstname';
+    public $lastNameField = '#customer_lastname';
+    public $passwordField = '#passwd';
+    public $birthDayField = '#days';
+    public $birthMonthField = '#months';
+    public $birthYearField = '#years';
+    public $addressField = '#address1';
+    public $cityField = '//*[@name="city"]';
+    public $stateField = 'select#id_state';
+    public $postcodeField = '//input[@id="postcode"]';
+    public $countryField = '#id_country';
+    public $mobilePhoneField = '#phone_mobile';
+    public $addressAliasField = '#alias';
+    public $registerAccountButton = '#submitAccount';
 
     /**
      * Declare UI map for this page here. CSS or XPath allowed.
@@ -30,6 +48,47 @@ class Cart
     public function __construct(\AcceptanceTester $I)
     {
         $this->acceptanceTester = $I;
+    }
+
+    public function createAccount(
+        $email,
+        $gender,
+        $firstName,
+        $lastName,
+        $password,
+        $birthDay,
+        $birthMonth,
+        $birthYear,
+        $address,
+        $city,
+        $state,
+        $postcode,
+        $country,
+        $mobilePhoneNumber,
+        $addressAlias)
+    {
+        $I = $this->acceptanceTester;
+        $I->click($this->signInButton);
+        $I->wait(2);
+        $I->fillField($this->emailField, $email);
+        $I->pressKey($this->emailField, WebDriverKeys::ENTER);
+        $I->wait(10);
+        $I->selectOption($this->mrOrMrsField, $gender);
+        $I->fillField($this->firstNameField, $firstName);
+        $I->fillField($this->lastNameField, $lastName);
+        $I->fillField($this->passwordField, $password);
+        $I->selectOption($this->birthDayField, $birthDay);
+        $I->selectOption($this->birthMonthField, $birthMonth);
+        $I->selectOption($this->birthYearField, $birthYear);
+        $I->fillField($this->addressField, $address);
+        $I->fillField($this->cityField, $city);
+        $I->selectOption($this->stateField, $state);
+        $I->fillField($this->postcodeField, $postcode);
+        $I->selectOption($this->countryField, $country);
+        $I->fillField($this->mobilePhoneField, $mobilePhoneNumber);
+        $I->fillField($this->addressAliasField, $addressAlias);
+        $I->click($this->registerAccountButton);
+        $I->wait(2);
     }
 
 }
